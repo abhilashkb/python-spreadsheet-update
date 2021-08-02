3e9b0387867022cd2b988b0c2c05a1c3451b45e8
abhilash <abhilashkb9020@gmail.com>
2021-08-03 00:03:34 +0530

    Task completed

--- spreadsheet-update.py
+++ spreadsheet-update.py
@@ -11,9 +11,23 @@ customer_orders=orderfile["Sheet3"]
 for customer in range(2,customer_loc.max_row+1):
     print(customer_loc.cell(customer,2).value)
     id=int(customer_loc.cell(customer,1).value) 
+    
+    ##Update phone number on Sheet1
+    
     for phone in range(2,customer_phone.max_row+1):
         if ( id == int(customer_phone.cell(phone,1).value)) :
             customer_loc.cell(customer,5).value=int(customer_phone.cell(phone,2).value)
-            
-            
+    
+    ##Update total purchase on Sheet1 and total cost on Sheet3
+    total_purchase = 0            
+    for order in range(2,customer_orders.max_row+1):
+        total_cost= int( customer_orders.cell(order,3).value * customer_orders.cell(order,4).value)
+        if ( id == int(customer_orders.cell(order,2).value)) :
+            total_purchase = total_purchase + total_cost
+        
+        customer_orders.cell(order,5).value = total_cost
+    customer_loc.cell(customer,6).value = total_purchase
+    
+        
+
 orderfile.save("solution.xlsx")
